c1435d96fd4e325d88d54f00f561e174d6c07647
asmalindi <asmalindi@gmail.com>
2011-07-23 16:21:56 +0000

    a news model has  chanched to Parcelable object. created a NewsmanagerApplication  to manage database

--- icta_news/android/src/com/icta/news/model/News.java
+++ icta_news/android/src/com/icta/news/model/News.java
@@ -1,11 +1,51 @@
 package com.icta.news.model;
 
-public class News {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class News implements Parcelable{
+	
+	private int id;
 	private String title;
 	private long created;
 	private String content;
 	private String ImageURL;
 	
+	public static final Parcelable.Creator<News> CREATOR = new Parcelable.Creator<News>() {
+
+		@Override
+		public News createFromParcel(Parcel source) {
+			return new News(source);
+		}
+
+		@Override
+		public News[] newArray(int size) {
+			return new News[size];
+		}
+	};
+	
+	
+	public News(Parcel in) {
+		readFromParcel(in);
+	}
+
+	public News(int id, String title, long created, String content,
+			String imageURL) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.created = created;
+		this.content = content;
+		ImageURL = imageURL;
+	}
+
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	public String getTitle() {
 		return title;
 	}
@@ -29,6 +69,32 @@ public class News {
 	}
 	public void setImageURL(String imageURL) {
 		ImageURL = imageURL;
+	}
+	
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public void writeToParcel(Parcel out, int flags) {
+		out.writeInt(id);
+		out.writeString(title);
+		out.writeLong(created);
+		out.writeString(content);
+		out.writeString(ImageURL);
+		
+	}
+	
+
+	private void readFromParcel(Parcel in) {
+		this.id = in.readInt();
+		this.title = in.readString();
+		this.created = in.readLong();
+		this.content = in.readString();
+		this.ImageURL = in.readString();
+		
 	}
 
 }
